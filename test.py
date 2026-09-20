8489328b88c319d526f8b5350514dc51e2d6be59
Lucie Flidrova <xflidrov@mail.muni.cz>
2026-09-20 17:12:34 +0200

    added test task

---
+++ test.py
@@ -0,0 +1,12 @@
+def my_two_to_the_power_of_n(n: int):
+    #TODO
+    pass
+
+
+print(my_two_to_the_power_of_n(0) == 1)
+print(my_two_to_the_power_of_n(1) == 2)
+print(my_two_to_the_power_of_n(2) == 4)
+print(my_two_to_the_power_of_n(8) == 256)
+print(my_two_to_the_power_of_n(10) == 1024)
+print(my_two_to_the_power_of_n(20) == 1048576)
+print(my_two_to_the_power_of_n(30) == 1073741824)
